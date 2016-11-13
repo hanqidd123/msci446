618db643754662194a248f5a1254dba9d59e1216
Han <q6han@nexus.uwaterloo.ca>
2016-11-13 15:08:20 -0500

    done

--- data_bk.py
+++ data_bk.py
@@ -1,45 +1,15 @@
 import openpyxl
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem.lancaster import LancasterStemmer
-stop_words = set(stopwords.words("english"))
+
 wb = openpyxl.load_workbook('clean_data.xlsx')
-sheet = wb.get_sheet_by_name("Sheet1")
-dataStr = wb.get_sheet_by_name("data")
-ps = LancasterStemmer()
-for x in range(1, 4481):
-    print(x)
-    sentence = sheet["D" + str(x)].value
-    tokens = nltk.word_tokenize(sentence)
-    print(tokens)
-    #sentences with no stop words
-    filtered_sentence_stopwords = []
+sheet = wb.get_sheet_by_name("data")
+dataStr = wb.get_sheet_by_name("matrix")
+discipline_list = []
 
-    for w in tokens:
-        if w not in stop_words:
-            filtered_sentence_stopwords.append(w)
-    #sentence after stemming
-    #filtered_sentence_stemmed = []
-    #for stem_words in filtered_sentence_stopwords:
-        #filtered_sentence_stemmed.append(ps.stem(stem_words))
-    #print(filtered_sentence_stemmed)
+for x in range(1,4481):
+    discipline_list.append(sheet.cell(row=2, column=x).value)
 
-    tagged = nltk.pos_tag(filtered_sentence_stopwords)
-    print(tagged)
-    #split_string = str(tagged)
-    count = 3
-    length = len(tagged)
-    dataStr.cell(row=1, column=x).value = sheet["B" + str(x)].value
-    dataStr.cell(row=2, column=x).value = sheet["A" + str(x)].value
-    for item in tagged:
-        dataStr.cell(row=count, column=x).value = str(item)
-        print(dataStr.cell(row=count, column=x).value)
-        count = count + 1
+discipline_list = list(set(discipline_list))
+discipline_list.sort()
 
-wb.save('clean_data.xlsx')
-
-
-
-
-
-
+print(discipline_list)
