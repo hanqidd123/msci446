06a02a7e0875e60e65bdd52d1d30dcadba9e0859
Han <q6han@nexus.uwaterloo.ca>
2016-11-20 15:40:24 -0500

    GG

--- NLTK.py
+++ NLTK.py
@@ -3,8 +3,6 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 
-#from nltk.stem.snowball import SnowballStemmer
-#from nltk.stem.lancaster import LancasterStemmer
 from nltk.tokenize import RegexpTokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
@@ -55,7 +53,8 @@ for x in range(1, 4481):
 
 
     #tagged = nltk.pos_tag(filtered_sentence_stopwords)
-    split_string = str(filtered_sentence_stemmed)
+
+
 
     unique_word_list.append(w)
     filtered_sentence_stopwords.sort()
@@ -69,47 +68,20 @@ for x in range(1, 4481):
     #filtered_sentence_stemmed = list(set(filtered_sentence_stemmed))
     #filtered_sentence_stemmed.sort()
 
-    tagged = nltk.pos_tag(filtered_sentence_stopwords)
-    split_string = str(tagged)
+    #tagged = nltk.pos_tag(filtered_sentence_stopwords)
+    #split_string = str(tagged)
 
-    count = 3
-    length = len(filtered_sentence_stemmed)
     dataStr.cell(row=1, column=x).value = sheet["B" + str(x)].value
-    dataStr.cell(row=2, column=x).value = sheet["A" + str(x)].value
-    for item in filtered_sentence_stemmed:
-        dataStr.cell(row=count, column=x).value = str(item)
-        count = count + 1
+    dataStr.cell(row=2, column=x).value = sheet["A" + str(x)].value.replace(" ","")
+    dataStr.cell(row=3, column=x).value = " ".join(filtered_sentence_stemmed)
+    print(dataStr.cell(row=2, column=x).value)
+
 
 
 #finding unique words
 print(original_word_list)
-unique_word_list = list(set(unique_word_list))
-unique_word_list.sort()
-count_x = 2
-print(unique_word_list)
 
-for item in unique_word_list:
-    matrix.cell(row=1, column=count_x).value = str(item)
-    count_x = count_x + 1
-
-discipline_list = []
-
-for x in range(1,4481):
-    discipline_list.append(dataStr.cell(row=2, column=x).value)
-
-discipline_list = list(set(discipline_list))
-
-count_y = 2
-for item in discipline_list:
-    matrix.cell(row=count_y, column=1).value = str(item)
-
-#discipline_list.sort()
-count_y = 2
-for item in discipline_list:
-    matrix.cell(row=1, column=count_y).value = str(item)
-
-    count_y = count_y + 1
-#wb.save('clean_data_test.xlsx')
+wb.save('clean_data_test.xlsx')
 
 
 
